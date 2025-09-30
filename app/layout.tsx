513fa1507e18f32a11e2718b2c0f500b9c051a20
HeatherRJohnson <36239217+HeatherRJohnson@users.noreply.github.com>
2025-09-29 17:50:31 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,9 @@
- (cd "$(git rev-parse --show-toplevel)" && git apply --3way <<'EOF' 
-diff --git a/app/layout.tsx b/app/layout.tsx
-index c90cf21932c5cdf395844bc1dbc4e82db752b3e0..2292f6ddc2efdd7a6c133e029d728b937bb12dce 100644
---- a/app/layout.tsx
-+++ b/app/layout.tsx
-@@ -1,7 +1,9 @@
--export default function RootLayout({ children }) {
-+import type { ReactNode } from 'react';
-+
-+export default function RootLayout({ children }: { children: ReactNode }) {
-   return (
-     <html lang="en">
-       <body style={{margin:0}}>{children}</body>
-     </html>
-   );
- }
- 
-EOF
-)
+import type { ReactNode } from 'react';
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body style={{margin:0}}>{children}</body>
+    </html>
+  );
+}
